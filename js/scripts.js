6a884a059ba1b830ab7f747a4f7997f4723ea6e8
Ryan Duff <ryanduff613@gmail.com>
2020-03-23 15:31:48 -0700

    correct initial error in js file

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,9 @@ function add(number1, number2) {
   return number1+number2;
   };
   
-  alert(add(10,5));
-  
   var number1 = parseInt(prompt("enter a number"));
   var number2 = parseInt(prompt('enter another number'));
-  
-  alert(add(number1,number2));
+  var result = add(number1,number2);
+
+  alert(result);
   
