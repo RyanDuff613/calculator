215a3d43ba2cc52ac06c2d58ce033a8bb73131ae
Ryan Duff <ryanduff613@gmail.com>
2020-03-23 15:39:30 -0700

    add functions for * / -

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,23 @@
 function add(number1, number2) {
   return number1+number2;
-  };
+};
+
+function subtract(number1, number2) {
+  return number1-number2;
+};
+
+function multiply(number1, number2)
+  return number1 * number2
+};
+
+function divide(number1,number2)
+  return number1 \ number2
+};
   
   var number1 = parseInt(prompt("enter a number"));
   var number2 = parseInt(prompt('enter another number'));
   var result = add(number1,number2);
 
   alert(result);
+  
   
